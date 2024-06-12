cb8503067054df00446145518754d198dbbde16d
woonzf <137765412+woonzf@users.noreply.github.com>
2024-06-12 23:39:40 +0800

    Refactor nav into ul, add a next button and adjust animate-menu

--- src/js/image.js
+++ src/js/image.js
@@ -3,3 +3,16 @@ import menu50w from "../img/icons/menu-50w.png";
 import menuW50w from "../img/icons/menu-w-50w.png";
 import close50w from "../img/icons/close-50w.png";
 import closeW50w from "../img/icons/close-w-50w.png";
+import chevron100w from "../img/icons/chevron-100w.png";
+import chevronW100w from "../img/icons/chevron-w-100w.png";
+
+const imgNextDark = document.querySelectorAll(".img-next-dark");
+const imgNext = document.querySelectorAll(".img-next");
+
+imgNextDark.forEach((img) => {
+  img.src = chevronW100w;
+});
+
+imgNext.forEach((img) => {
+  img.src = chevron100w;
+});

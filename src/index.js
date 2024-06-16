000bbda663f17c83c294b1203c572ab099350c08
woonzf <137765412+woonzf@users.noreply.github.com>
2024-06-16 14:34:36 +0800

    Add a tools carousel to ARTICLE-ABOUT and a tool to ARTICLE-ABOUT-MORE

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import "./js/image";
 import { darkMode } from "./js/dark-mode";
 import { menu } from "./js/menu";
 import { scroll } from "./js/scroll";
+import { carousel } from "./js/carousel";
 
 darkMode.init();
 menu.init();
 scroll.init();
+carousel.init();
---
+++ src/js/tools.js
@@ -0,0 +1,95 @@
+const tools = (() => {
+  const list = [
+    {
+      src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
+      alt: "HTML",
+    },
+    {
+      src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg",
+      alt: "CSS",
+    },
+    {
+      src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+      alt: "JavaScript",
+    },
+    {
+      src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/webpack/webpack-original.svg",
+      alt: "Webpack",
+    },
+    {
+      src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jest/jest-plain.svg",
+      alt: "Jest",
+    },
+    {
+      src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+      alt: "Tailwind CSS",
+    },
+    {
+      src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg",
+      alt: "C",
+    },
+    {
+      src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg",
+      alt: "Python",
+    },
+    {
+      src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sqlite/sqlite-original.svg",
+      alt: "SQLite",
+    },
+    {
+      class: ["devicon-flask-original", "colored"],
+      alt: "Flask",
+    },
+    {
+      src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg",
+      alt: "Bootstrap",
+    },
+    {
+      src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vscode/vscode-original.svg",
+      alt: "Visual Studio Code",
+    },
+    {
+      src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg",
+      alt: "Git",
+    },
+    {
+      class: ["devicon-github-original", "colored"],
+      alt: "GitHub",
+    },
+  ];
+
+  function getList() {
+    return get(
+      "SQLite",
+      "Flask",
+      "Bootstrap",
+      "Visual Studio Code",
+      "Git",
+      "GitHub",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Webpack",
+      "Jest",
+      "Tailwind CSS",
+      "C",
+      "Python",
+    );
+  }
+
+  function get() {
+    let getList = [];
+
+    for (const argument of arguments) {
+      for (const tool of list) {
+        if (argument === tool.alt) getList.push(tool);
+      }
+    }
+
+    return getList;
+  }
+
+  return { getList, get };
+})();
+
+export { tools };

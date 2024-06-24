f3eae3b137b06c4ef06f08c57d85ff01f9240d05
woonzf <137765412+woonzf@users.noreply.github.com>
2024-06-24 20:25:37 +0800

    Add a 4th project card for accessing more projects on GitHub and adjust carousel to accomodate the new tab

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -7,10 +7,12 @@ const carousel = (() => {
 
   const projectCard1 = document.querySelector("#project-card-1");
   const projectCard2 = document.querySelector("#project-card-2");
+  const projectCard3 = document.querySelector("#project-card-3");
 
   const gap = 16;
   let scrollWidthProjectCard1 = 0;
   let scrollWidthProjectCard2 = 0;
+  let scrollWidthProjectCard3 = 0;
 
   let i = 1;
 
@@ -19,7 +21,7 @@ const carousel = (() => {
 
     btnProjectNext.onclick = () => {
       document.querySelector(`#btn-mini-tab-${i}`).classList.toggle("active");
-      if (i === 3) {
+      if (i === 4) {
         projectCardWrapper.scrollTo(0, 0);
         i = 1;
       } else {
@@ -32,8 +34,8 @@ const carousel = (() => {
     btnProjectPrev.onclick = () => {
       document.querySelector(`#btn-mini-tab-${i}`).classList.toggle("active");
       if (i === 1) {
-        projectCardWrapper.scrollTo(scrollWidthProjectCard2 + 1, 0);
-        i = 3;
+        projectCardWrapper.scrollTo(scrollWidthProjectCard3 + 1, 0);
+        i = 4;
       } else {
         projectCardWrapper.scrollBy(-scrollWidthProjectCard1, 0);
         i -= 1;
@@ -50,7 +52,12 @@ const carousel = (() => {
         scroll < scrollWidthProjectCard2
       )
         i = 2;
-      else i = 3;
+      else if (
+        scroll >= scrollWidthProjectCard2 &&
+        scroll < scrollWidthProjectCard3
+      )
+        i = 3;
+      else i = 4;
       document.querySelector(`#btn-mini-tab-${i}`).classList.toggle("active");
     };
 
@@ -75,6 +82,8 @@ const carousel = (() => {
     scrollWidthProjectCard1 = projectCard1.clientWidth + gap;
     scrollWidthProjectCard2 =
       projectCard2.clientWidth + gap + scrollWidthProjectCard1;
+    scrollWidthProjectCard3 =
+      projectCard3.clientWidth + gap + scrollWidthProjectCard2;
   }
 
   return { init, getScrollWidthProjectCard };

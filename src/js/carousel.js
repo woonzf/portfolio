745247901f46d577b04098d5dcc8aa4180d4a578
woonzf <137765412+woonzf@users.noreply.github.com>
2024-07-02 15:27:19 +0800

    Clean carousel codes and make countdown bar timing more synchronized with the carousel cycle and adjust project card index md: position

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -4,6 +4,9 @@ const carousel = (() => {
 
   const projectCardWrapper = document.querySelector(".project-card-wrapper");
   const btnMiniTabs = document.querySelectorAll(".btn-mini-tab");
+  const carouselCountdownBar = document.querySelector(
+    "#carousel-countdown-bar",
+  );
 
   let scrollWidthProjectCards = null;
 
@@ -18,10 +21,7 @@ const carousel = (() => {
       if (i === 4) i = 1;
       else i += 1;
       projectCardWrapper.scrollTo((scrollWidthProjectCards * (i - 1)) / 4, 0);
-      document
-        .querySelector(`#btn-mini-tab-${iPrev}`)
-        .classList.toggle("active");
-      document.querySelector(`#btn-mini-tab-${i}`).classList.toggle("active");
+      _toggleBtnMiniTabs(iPrev, i);
     };
 
     btnProjectPrev.onclick = () => {
@@ -29,10 +29,7 @@ const carousel = (() => {
       if (i === 1) i = 4;
       else i -= 1;
       projectCardWrapper.scrollTo((scrollWidthProjectCards * (i - 1)) / 4, 0);
-      document
-        .querySelector(`#btn-mini-tab-${iPrev}`)
-        .classList.toggle("active");
-      document.querySelector(`#btn-mini-tab-${i}`).classList.toggle("active");
+      _toggleBtnMiniTabs(iPrev, i);
     };
 
     projectCardWrapper.onscrollend = () => {
@@ -50,10 +47,7 @@ const carousel = (() => {
       )
         i = 3;
       else i = 4;
-      document
-        .querySelector(`#btn-mini-tab-${iPrev}`)
-        .classList.toggle("active");
-      document.querySelector(`#btn-mini-tab-${i}`).classList.toggle("active");
+      _toggleBtnMiniTabs(iPrev, i);
     };
 
     btnMiniTabs.forEach((btn) => {
@@ -61,12 +55,11 @@ const carousel = (() => {
         iPrev = i;
         i = +btn.id.slice(-1);
         projectCardWrapper.scrollTo((scrollWidthProjectCards * (i - 1)) / 4, 0);
-        document
-          .querySelector(`#btn-mini-tab-${iPrev}`)
-          .classList.toggle("active");
-        document.querySelector(`#btn-mini-tab-${i}`).classList.toggle("active");
+        _toggleBtnMiniTabs(iPrev, i);
       };
     });
+
+    carouselCountdownBar.classList.add("animate-shrink-width");
 
     setInterval(() => {
       btnProjectNext.click();
@@ -75,6 +68,11 @@ const carousel = (() => {
 
   function getScrollWidthProjectCard() {
     scrollWidthProjectCards = projectCardWrapper.scrollWidth;
+  }
+
+  function _toggleBtnMiniTabs(iPrev, i) {
+    document.querySelector(`#btn-mini-tab-${iPrev}`).classList.toggle("active");
+    document.querySelector(`#btn-mini-tab-${i}`).classList.toggle("active");
   }
 
   return { init, getScrollWidthProjectCard };
